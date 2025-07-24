c6d677a50a776c2537fb108bd80ea38fb1fc3a38
6enard <109796647+6enard@users.noreply.github.com>
2025-07-24 22:53:22 +0300

    Updated ProjectDetail.tsx

--- src/components/ProjectDetail.tsx
+++ src/components/ProjectDetail.tsx
@@ -55,7 +55,18 @@ const ProjectDetail: React.FC<ProjectDetailProps> = ({ projectId, onClose }) => 
       volunteers: "20 volunteers",
       status: "Completed",
       gallery: [
-        "/street1.jpg",
+        "/1.jpg",
+        "/2.jpg",
+        "/3.jpg",
+        "/4.jpg",
+        "/5.jpg",
+        "/6.jpg",
+        "/7.jpg",
+        "/8.jpg",
+        "/9.jpg",
+        "/10.jpg",
+        "/11.jpg",
+        "/12.jpg"
       ]
     },
     {
