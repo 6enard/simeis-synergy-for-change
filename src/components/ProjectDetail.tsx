df24dd0660e030c665e4664b5f79ddcb85c0cb45
6enard <109796647+6enard@users.noreply.github.com>
2025-09-26 11:25:48 +0300

    Updated ProjectDetail.tsx

--- src/components/ProjectDetail.tsx
+++ src/components/ProjectDetail.tsx
@@ -224,7 +224,7 @@ const ProjectDetail: React.FC<ProjectDetailProps> = ({ projectId, onClose }) => 
       status: "Completed",
       category: "children",
       gallery: [
-        "/ssc/IMG-20250926-WA0004.jpg",
+        "/ssc/IMG-20250926-WA00063.jpg",
         "/ssc/IMG-20250926-WA0005.jpg",
         "/ssc/IMG-20250926-WA0006.jpg",
         "/ssc/IMG-20250926-WA0007.jpg",
@@ -283,7 +283,7 @@ const ProjectDetail: React.FC<ProjectDetailProps> = ({ projectId, onClose }) => 
         "/ssc/IMG-20250926-WA0060.jpg",
         "/ssc/IMG-20250926-WA0061.jpg",
         "/ssc/IMG-20250926-WA0062.jpg",
-        "/ssc/IMG-20250926-WA0063.jpg",
+        "/ssc/IMG-20250926-WA004.jpg",
         "/ssc/IMG-20250926-WA0064.jpg",
         "/ssc/IMG-20250926-WA0065.jpg",
         "/ssc/IMG-20250926-WA0066.jpg",
